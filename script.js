5fa2fa49ee3245fb4794ac2b36e39857281f6dd2
shazomii <seyeoyeniran2u@gmail.com>
2025-05-19 19:52:14 +0100

    Add calculator operation functions

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operand1, operand2, operator) {
+    switch (operator) {
+        case "+":
+            return add(operand1, operand2);
+        case "-":
+            return subtract(operand1, operand2);
+        case "*":
+            return multiply(operand1, operand2);
+        case "/":
+            if (operand2 == 0) {
+                return 0;
+            } else {
+                return divide(operand1, operand2);
+            }
+    }
+}
